c96da1494feee2024b4dea7da1bcdfba35301850
majiang <majiang@users.noreply.github.com>
2025-02-11 18:02:09 +0900

    Sort and validate

--- src/gb/pws.py
+++ src/gb/pws.py
@@ -5,11 +5,12 @@ import gb
 
 
 def process(dir_in: str):
-    for filename in glob.glob(
+    for filename in sorted(glob.glob(
         os.path.join(
             dir_in, "gb", "pws", "*", "*.json"  # rule  # regulation  # year?  # date?
         )
-    ):
-        set_score = gb.read_json_validate(filename)
-        result = gb.DayResult.from_set_score(set_score)
-        yield filename, set_score, result
+    )):
+        input_score = gb.from_json_file(filename)
+        validated_score = input_score.validate()
+        result = gb.DayResult.from_set_score(validated_score)
+        yield filename, validated_score, result
